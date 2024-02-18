be4625d5a1c34f03646906dbbda0b782e8b65302
Santan Reddy <santanreddy171@gmail.com>
2024-02-18 21:59:59 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Popup from './components/Popup.vue'
+import {ref,onMounted} from 'vue'
+
+const triggers = ref({
+  buttonsTrigger:false,
+  timedTrigger:false
+})
+onMounted(() => {
+  setTimeout(() => {
+    triggers.value.timedTrigger = true
+  }, 3000)
+})
+const toggleTrigger=(trigger)=>{
+  triggers.value[trigger]=!triggers.value[trigger]
+}
+</script>
+
+<template>
+  <main>
+    <h1>Vue Popup</h1>
+    <button @click="toggleTrigger('buttonsTrigger')">Open Popup</button>
+    <Popup
+     v-if="triggers.buttonsTrigger"
+     :toggleTrigger="toggleTrigger('buttonsTrigger')"
+     >
+      <h2>My Popup</h2>
+    </Popup>
+    <Popup v-if="triggers.timedTrigger">
+      <h2>My Popup</h2>
+    </Popup>
+  </main>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Popup.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="popup">
+        <div class="popup-inner">
+            <slot />
+            <button @click="toggleTrigger('buttonsTrigger')" class="popup-close">Close Popup</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const props = defineProps({
+    toggleTrigger:Function
+})
+</script>
+
+<style scoped lang="scss">
+.popup{
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+    z-index: 9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .popup-inner{
+        background-color: white;
+        padding: 2rem;
+    }
+}
+</style>
+
